e1bbd2086221d3f70e8a7a2e1d9cf77d6c64679f
iam <iam@hackessy>
2026-08-22 13:03:16 -0400

    Feature: Built All Posts b4 Deploying

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '24c9fb3a837222d68eab6c535dbd3bb2fba745dd', queries,  });
+export const client = createClient({ cacheDir: '/home/iam/magical-moon/tina/__generated__/.cache/1787418067518', url: 'https://content.tinajs.io/2.4/content/0e3d0092-9e76-4c12-afde-a0ec6390907c/github/main', token: '24c9fb3a837222d68eab6c535dbd3bb2fba745dd', queries,  });
 export default client;
   
